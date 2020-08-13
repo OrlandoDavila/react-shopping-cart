9c5e1a05aa139e97781c1c7d272334d320066fb1
Orlando Davila <odavila0308@gmail.com>
2020-08-13 16:11:59 -0400

    MVP complete

--- src/App.js
+++ src/App.js
@@ -2,32 +2,52 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//Contexts
+import {ProductContext} from './contexts/ProductContext'
+import {CartContext} from './contexts/CartContext'
+
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState([]); //Its an array
 
 	const addItem = item => {
 		// add the given item to the cart
-	};
+		console.log(item)
+	return setCart([item])
+	//cart is an array
+	
+	
+	//holds item details in a object
 
+	};
 	return (
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value= {{cart}} >
 		<div className="App">
+
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+			
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				{/* <Products products={products} addItem={addItem} /> */}
+				<Products />
+
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+			{/* <ShoppingCart cart={cart} /> */}
+				<ShoppingCart/>
 			</Route>
 		</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,49 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import {ProductContext} from '../contexts/ProductContext'
+ 
 // Components
 import Product from './Product';
 
-const Products = props => {
+const Products = () => {
+	const { products, addItem } = useContext(ProductContext);
+
 	return (
+		
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => ( //data.map useState
 				<Product
+				
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
+					//addItem(product)
 				/>
+				
+				
 			))}
 		</div>
 	);
 };
+
+
+//*Pre-Context */
+
+// const Products = props => {
+// 	return (
+// 		<div className="products-container">
+// 			{props.products.map(product => ( //data.map useState
+// 				<Product
+				
+// 					key={product.id}
+// 					product={product}
+// 					addItem={props.addItem}
+// 					//addItem(product)
+// 				/>
+				
+				
+// 			))}
+// 		</div>
+// 	);
+// };
 
 export default Products;
